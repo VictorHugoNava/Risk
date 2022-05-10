7e40e1015859cdf61f9f669ab9d902051f407755
VictorHugo <victorhugo@VictorHugos-MacBook-Pro.local>
2022-05-10 15:16:14 -0700

    Init commit project

---
+++ app/src/main/java/com/example/riskapp/MultiAttackScreen.java
@@ -0,0 +1,14 @@
+package com.example.riskapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MultiAttackScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_attack_screen);
+    }
+}
